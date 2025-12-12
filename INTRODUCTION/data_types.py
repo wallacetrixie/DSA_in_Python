0c94b828b49f8154fd3f4a499601aa2cc6a05771
wallacetrixie <wallacewambulwa@gmail.com>
2025-12-12 13:36:58 +0300

    Basic data types

---
+++ INTRODUCTION/data_types.py
@@ -0,0 +1,6 @@
+#Basic data types and variables in python
+Age=21
+Name="Wallace Wambula"
+Plan="Master Python prgraming language"
+Height= 5.5
+print("I am ",Name ,"Aged",Age,"Years Old and my height is ",Height,"Feet")
