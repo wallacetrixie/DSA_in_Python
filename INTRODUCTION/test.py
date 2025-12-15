d99911574b34544eab3e2d5de080bb4d7ee1bcdb
wallacetrixie <wallacewambulwa@gmail.com>
2025-12-15 21:15:59 +0300

    prime number

--- INTRODUCTION/test.py
+++ INTRODUCTION/test.py
@@ -1,3 +1,9 @@
-number=int(input("Enter a number: "))
-result="even " if number%2==0 else "odd"
-print(result)
+#prints prime numbers
+for num in range(0, 20):
+	is_prime = True
+	for i in range(2, int(num ** 0.5) + 1):
+		if num % i == 0:
+			is_prime = False
+			break
+	if is_prime:
+		print(num)
